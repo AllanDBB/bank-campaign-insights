38985bc2e28e131599aad24be2a61651bb803a1d
Helena0504 <helenamaria.vargasq@gmail.com>
2025-11-26 12:44:45 -0600

    Login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,8 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/" element={<DataLoad />} />
-      <Route path="/app" element={<App />} />
       <Route 
         path="/" 
         element={
