ce6bbf1cdbb7e19755ee78b0d80e287f0825d62c
Helena0504 <helenamaria.vargasq@gmail.com>
2025-11-27 12:33:55 -0600

    Update export.js

--- server/src/routes/export.js
+++ server/src/routes/export.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import extractUserId from '../middleware/extractUserId.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 import ExportController from '../controllers/ExportController.js';
 
 const router = express.Router();
